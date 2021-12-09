38a65148b9474ca335c8877f9dafe5d7af9ad58f
Kailash-ayer <95143961+Kailash-ayer@users.noreply.github.com>
2021-12-09 16:02:42 +0530

    emp

---
+++ Student_details_api_core/Models/Employee.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Student_details_api_core.Models
+{
+    public class Employee
+    {
+    }
+}
