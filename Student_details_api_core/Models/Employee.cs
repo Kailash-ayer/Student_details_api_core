a5fae1250607529be40288d28f6081b4dd59dfa1
Kailash-ayer <95143961+Kailash-ayer@users.noreply.github.com>
2021-12-09 15:29:03 +0530

    Add project files.

---
+++ Student_details_api_core/Models/Employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Student_details_api_core.Models
+{
+    public class Employee
+    {   [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Fname { get; set; }
+        [Required]
+        public string Lname { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Mobile { get; set; }
+        [Required]
+        public string Gender { get; set; }
+        [Required]
+        public string Address { get; set; }
+    }
+}
---
+++ Student_details_api_core/Repositories/StudentsRepository.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Student_details_api_core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Student_details_api_core.Repositories
+{
+    public class StudentsRepository : IStudentsRepository
+    {
+        //constructor
+        public readonly StudentContext _context;
+        public StudentsRepository(StudentContext context)
+        {
+            this._context = context;
+        }
+     
+
+        public async Task<Students> Create(Students students)
+        {
+            _context.Student.Add(students);
+            await _context.SaveChangesAsync();
+            return students;
+        }
+
+        public async Task Delete(int id)
+        {
+            var studentToDelete = await _context.Student.FindAsync(id);
+            _context.Student.Remove(studentToDelete);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<Students>> Get()
+        {
+            return await _context.Student.ToListAsync();
+        }
+
+        public async Task<Students> Get(int id)
+        {
+            return await _context.Student.FindAsync(id);
+        }
+
+        public async Task Update(Students students)
+        {
+            _context.Entry(students).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+        }
+    }
+}
